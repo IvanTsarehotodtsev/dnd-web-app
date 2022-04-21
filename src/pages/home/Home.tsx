3eda05184c10775f64de89790186f195ddebd815
Ivan Tsarehorodtsev <i.tsarehorodtsev@kasta.software>
2022-04-21 17:57:12 +0300

    add ui lib

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from '@mui/material/Button';
 
 const Home: React.FC = () => {
     return (
         <div>
             Home page
+
+            <Button variant="contained">Hello World</Button>
         </div>
     )
 }
