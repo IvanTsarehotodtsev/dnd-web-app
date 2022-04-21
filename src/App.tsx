d517854283a462f0a388dfb3ee3a554d5f61f341
Ivan Tsarehorodtsev <i.tsarehorodtsev@kasta.software>
2022-04-21 17:52:29 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './pages/home/Home';
+
+import './App.scss';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
